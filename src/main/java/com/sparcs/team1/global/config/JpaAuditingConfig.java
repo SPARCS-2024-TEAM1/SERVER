3e349667b22c02c92936b4fb1995a6b1a47a2ca6
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-29 19:59:41 +0900

    [FEAT/#1] JpaAuditing 설정 파일 추가

---
+++ src/main/java/com/sparcs/team1/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.sparcs.team1.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
