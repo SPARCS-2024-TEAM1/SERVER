df4258516a7be73d249fbd52fd5bc0c58751d2d0
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-29 19:59:30 +0900

    [SETTING/#1] 프로젝트 초기 세팅

---
+++ src/main/java/com/sparcs/team1/Team1Application.java
@@ -0,0 +1,12 @@
+package com.sparcs.team1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team1Application.class, args);
+    }
+}
