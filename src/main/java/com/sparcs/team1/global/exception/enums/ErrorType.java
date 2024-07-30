110e6df7505f1cad214995e4ff8f98988ffcf611
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-30 15:55:59 +0900

    [FEAT/#7] 커스텀 에러 타입 추가

--- src/main/java/com/sparcs/team1/global/exception/enums/ErrorType.java
+++ src/main/java/com/sparcs/team1/global/exception/enums/ErrorType.java
@@ -13,6 +13,15 @@ public enum ErrorType {
      * 400 BAD REQUEST
      */
     REQUEST_VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "40001", "유효하지 않은 요청입니다.(Valid 실패)"),
+    INVALID_PHONE_NUMBER_ERROR(HttpStatus.BAD_REQUEST, "40002", "잘못된 휴대전화 번호 양식입니다."),
+    INVALID_VERIFICATION_CODE_ERROR(HttpStatus.BAD_REQUEST, "40003", "인증번호가 올바르지 않습니다."),
+    INVALID_NICKNAME_ERROR(HttpStatus.BAD_REQUEST, "40004", "닉네임이 조건을 만족하지 않습니다."),
+
+    /**
+     * 404 NOT FOUND
+     */
+    NOT_FOUND_VERIFICATION_REQUEST_HISTORY(HttpStatus.NOT_FOUND, "40401", "인증 요청 이력이 존재하지 않습니다."),
+    NOT_FOUND_MEMBER_ERROR(HttpStatus.NOT_FOUND, "40402", "존재하지 않는 회원입니다."),
     ;
 
     private final HttpStatus httpStatus;
