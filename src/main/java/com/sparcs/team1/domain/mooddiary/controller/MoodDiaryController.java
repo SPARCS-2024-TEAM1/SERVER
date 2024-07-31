4eb753ca692709eafd76b9594302e3986c67cde2
ckkim817 <ckkim817@konkuk.ac.kr>
2024-08-01 01:56:14 +0900

    [FEAT/#17] 챗봇 답변 생성 관련 엔드포인트 구현

--- src/main/java/com/sparcs/team1/domain/mooddiary/controller/MoodDiaryController.java
+++ src/main/java/com/sparcs/team1/domain/mooddiary/controller/MoodDiaryController.java
@@ -1,5 +1,7 @@
 package com.sparcs.team1.domain.mooddiary.controller;
 
+import com.sparcs.team1.domain.mooddiary.dto.CreateAnswerRequest;
+import com.sparcs.team1.domain.mooddiary.dto.CreateAnswerResponse;
 import com.sparcs.team1.domain.mooddiary.dto.CreateDiaryRequest;
 import com.sparcs.team1.domain.mooddiary.dto.CreateDiaryResponse;
 import com.sparcs.team1.domain.mooddiary.service.MoodDiaryService;
@@ -7,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,12 @@ public class MoodDiaryController implements MoodDiaryApi {
             @ModelAttribute CreateDiaryRequest createDiaryRequest
     ) {
         return ResponseEntity.ok(moodDiaryService.createMoodDiary(createDiaryRequest));
+    }
+
+    @PostMapping("/answer")
+    public ResponseEntity<CreateAnswerResponse> createAnswer(
+            @RequestBody CreateAnswerRequest createAnswerRequest
+    ) {
+        return ResponseEntity.ok(moodDiaryService.createAnswer(createAnswerRequest));
     }
 }
