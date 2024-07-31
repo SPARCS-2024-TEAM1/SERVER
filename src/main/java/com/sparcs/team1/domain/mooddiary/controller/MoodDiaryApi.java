8042b9af83becc8fb71a82e4f929fbd9e2d36c73
ckkim817 <ckkim817@konkuk.ac.kr>
2024-08-01 01:57:12 +0900

    [FEAT/#17] 챗봇 답변 생성 API 스웨거 추가

--- src/main/java/com/sparcs/team1/domain/mooddiary/controller/MoodDiaryApi.java
+++ src/main/java/com/sparcs/team1/domain/mooddiary/controller/MoodDiaryApi.java
@@ -1,5 +1,7 @@
 package com.sparcs.team1.domain.mooddiary.controller;
 
+import com.sparcs.team1.domain.mooddiary.dto.CreateAnswerRequest;
+import com.sparcs.team1.domain.mooddiary.dto.CreateAnswerResponse;
 import com.sparcs.team1.domain.mooddiary.dto.CreateDiaryRequest;
 import com.sparcs.team1.domain.mooddiary.dto.CreateDiaryResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,8 +13,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@Tag(name = "Mood Diary Api") // 임시
+@Tag(name = "감정 기록 API")
 public interface MoodDiaryApi {
 
     @Operation(
@@ -59,5 +62,46 @@ public interface MoodDiaryApi {
     @PostMapping("/diary")
     public ResponseEntity<CreateDiaryResponse> createMoodDiary(
             @ModelAttribute CreateDiaryRequest createDiaryRequest
+    );
+
+    @Operation(
+            summary = "챗봇 답변 생성 API",
+            description = "사용자의 기록을 바탕으로 선택한 어시스턴트한테 적절한 답변을 받습니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Success",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            implementation = CreateAnswerResponse.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "40006",
+                            description = "응답 값을 객체로 변환하는 데 실패하였습니다.",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "40403",
+                            description = "Response Body가 존재하지 않습니다.",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "40404",
+                            description = "존재하지 않는 감정 기록입니다.",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "50001",
+                            description = "FEIGN 에러가 발생하였습니다.",
+                            content = @Content
+                    )
+            }
+    )
+    @PostMapping("/answer")
+    public ResponseEntity<CreateAnswerResponse> createAnswer(
+            @RequestBody CreateAnswerRequest createAnswerRequest
     );
 }
