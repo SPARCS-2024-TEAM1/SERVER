2408f609bcf22d01e506b08fa7fee621fae246a1
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-31 11:18:52 +0900

    [FEAT/#11] FeignErrorDecoder 구현

--- src/main/java/com/sparcs/team1/global/exception/enums/ErrorType.java
+++ src/main/java/com/sparcs/team1/global/exception/enums/ErrorType.java
@@ -17,12 +17,19 @@ public enum ErrorType {
     INVALID_VERIFICATION_CODE_ERROR(HttpStatus.BAD_REQUEST, "40003", "인증번호가 올바르지 않습니다."),
     INVALID_NICKNAME_ERROR(HttpStatus.BAD_REQUEST, "40004", "닉네임이 조건을 만족하지 않습니다."),
     FAILED_FILE_UPLOAD_ERROR(HttpStatus.BAD_REQUEST, "40005", "파일 업로드에 실패하였습니다."),
+    FAILED_REQUEST_BODY_PARSING_ERROR(HttpStatus.BAD_REQUEST, "40006", "응답 값을 객체로 변환하는 데 실패하였습니다."),
 
     /**
      * 404 NOT FOUND
      */
     NOT_FOUND_VERIFICATION_REQUEST_HISTORY(HttpStatus.NOT_FOUND, "40401", "인증 요청 이력이 존재하지 않습니다."),
     NOT_FOUND_MEMBER_ERROR(HttpStatus.NOT_FOUND, "40402", "존재하지 않는 회원입니다."),
+    NOT_FOUND_RESPONSE_BODY_ERROR(HttpStatus.NOT_FOUND, "40403", "Response Body가 존재하지 않습니다."),
+
+    /**
+     * 500 INTERNAL SERVER ERROR
+     */
+    INTERNAL_FEIGN_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "50001", "FEIGN 에러가 발생하였습니다."),
     ;
 
     private final HttpStatus httpStatus;
