1253f1ed538965c7def672856406fa336944313a
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-31 18:42:16 +0900

    [CORRECT/#13] Mood Enum 값 한글로 변경
    클라 요청

--- src/main/java/com/sparcs/team1/domain/mooddiary/model/Mood.java
+++ src/main/java/com/sparcs/team1/domain/mooddiary/model/Mood.java
@@ -8,15 +8,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum Mood {
 
-    JOY("JOY"),
-    ANXIETY("ANXIETY"),
-    STRESS("STRESS"),
-    SADNESS("SADNESS"),
-    CALMNESS("CALMNESS"),
-    GRATITUDE("GRATITUDE"),
-    ANGER("ANGER"),
-    FATIGUE("FATIGUE"),
-    LONELINESS("LONELINESS"),
+    기쁨("기쁨"),
+    불안("불안"),
+    스트레스("스트레스"),
+    슬픔("슬픔"),
+    평온("평온"),
+    감사("감사"),
+    화남("화남"),
+    피로("피로"),
+    외로움("외로움"),
     ;
 
     private final String mood;
