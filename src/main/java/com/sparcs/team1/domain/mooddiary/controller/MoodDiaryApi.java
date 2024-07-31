9bcf7e8c9edfbdfef8b3bd108f80c2be5f5d925d
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-31 11:33:57 +0900

    [FEAT/#11] Speech-To-Summary-Text API 스웨거 추가

--- src/main/java/com/sparcs/team1/domain/mooddiary/controller/MoodDiaryApi.java
+++ src/main/java/com/sparcs/team1/domain/mooddiary/controller/MoodDiaryApi.java
@@ -16,8 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 public interface MoodDiaryApi {
 
     @Operation(
-            summary = "Speech-To-Text API",
-            description = "네이버 클라우드 플랫폼의 Clova Speech를 이용해 음성을 텍스트로 변환합니다.",
+            summary = "Speech-To-Summary-Text API",
+            description = "네이버 클라우드 플랫폼의 Clova Speech와 Clova Studio를 이용해 음성을 텍스트 요약본으로 변환합니다.",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
@@ -35,8 +35,23 @@ public interface MoodDiaryApi {
                             content = @Content
                     ),
                     @ApiResponse(
+                            responseCode = "40006",
+                            description = "응답 값을 객체로 변환하는 데 실패하였습니다.",
+                            content = @Content
+                    ),
+                    @ApiResponse(
                             responseCode = "40402",
                             description = "존재하지 않는 회원입니다.",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "40403",
+                            description = "Response Body가 존재하지 않습니다.",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "50001",
+                            description = "FEIGN 에러가 발생하였습니다.",
                             content = @Content
                     )
             }
